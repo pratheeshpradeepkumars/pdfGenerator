51511edf72e8fd0664e08c0116e2535a00f5cbe6
pratheesh.p <pratheesh.p@appviewx.com>
2019-05-31 15:27:33 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import './App.css';
+import axios from 'axios';
+import { saveAs } from 'file-saver';
+
+class App extends React.Component  {
+    state = {  
+        name: 'Adrian',   
+        receiptId: 0,   
+        price1: 0,   
+        price2: 0,
+    };
+    handleChange = ({ target: { value, name } }) => {
+        this.setState({ [name]: value });
+    };
+    downloadPDF = () => {
+         axios.post('/create-pdf', this.state)
+         .then(() => axios.get('/fetch-pdf', { responseType: 'blob' }))
+         .then((res) => {   
+            const pdfBlob = new Blob([res.data], { type: 'application/pdf' });
+            saveAs(pdfBlob, 'generatedDocument.pdf')
+        });
+    };
+    render() {
+        return (
+            <div className="App">
+                <input type="text" 
+                    placeholder="Name" 
+                    name="name" 
+                    onChange = {this.handleChange}
+                />   
+                <input type="number" 
+                    placeholder="Receipt ID" 
+                    name="receiptId"    
+                    onChange={this.handleChange}
+                />
+                <input 
+                    type="number" 
+                    placeholder="Price 1" 
+                    name="price1" 
+                    onChange = {this.handleChange}
+                />   
+                <input 
+                type="number" 
+                placeholder="Price 2" 
+                name="price2" 
+                onChange={this.handleChange}
+                />   
+                <button 
+                onClick={this.downloadPDF}>
+                    Download PDF
+                </button>
+            </div>
+        );
+        
+    }
+}
+
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const pdf = require('html-pdf');
+const cors = require('cors');
+const pdfTemplate = require('./documents/index.js');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+app.post('/create-pdf', (req, res) => { 
+    pdf.create(pdfTemplate(req.body), {})
+    .toFile('rezultati.pdf', (err) => {
+        if(err) {
+            return console.log('error');
+        }
+        res.send(Promise.resolve())  
+    });
+});
+
+app.get('/fetch-pdf', (req, res) => { 
+    res.sendFile(`${__dirname}/rezultati.pdf`);
+});
