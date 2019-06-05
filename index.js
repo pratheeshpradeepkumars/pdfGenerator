f6f46f5e7486e482eb563b5377e261fef969e572
pratheesh.p <pratheesh.p@appviewx.com>
2019-06-05 18:56:58 +0530

    changes

--- index.js
+++ index.js
@@ -17,6 +17,14 @@ async function printPDF() {
 
 
 const app = express();
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static('client/build'));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, client, 'build', 'index.html'));
+    });
+}
+
 const port = process.env.PORT || 5000;
 
 app.use(cors());
